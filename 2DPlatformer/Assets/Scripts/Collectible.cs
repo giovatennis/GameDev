d7fdf36117f946451ef32e27b0174b2fb0bbf708
giovatennis <67833118+giovatennis@users.noreply.github.com>
2026-05-30 23:33:45 -0500

    2

---
+++ 2DPlatformer/Assets/Scripts/Collectible.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerMovement>().Collect();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2DPlatformer/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float moveDistance = 3f;
+
+    Vector3 startingPosition;
+
+    int direction = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+        float distanceFromStart = transform.position.x - startingPosition.x;
+
+        if(Mathf.Abs(distanceFromStart) >= moveDistance)
+        {
+            direction *= -1;
+        }
+    }
+}
